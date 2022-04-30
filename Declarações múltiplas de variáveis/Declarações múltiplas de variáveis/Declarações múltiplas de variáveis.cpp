c0b46a5069669d762a12d7f1ff9cf9dcdc732cc1
Allan <allanfelipedk@gmail.com>
2022-04-29 22:28:16 -0300

    Declarações múltiplas de variáveis, Constantes - #Define

---
+++ Declarações múltiplas de variáveis/Declarações múltiplas de variáveis/Declarações múltiplas de variáveis.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415 << "\n";
+#define myName cout << "My name is Allan \n\n";
+int main()
+{
+    int vida = 100, shield = 50;
+    cout << pi;
+    myName
+
+
+    system("pause>0");
+}
