29454142a199201920e9515816a0c0dd420dbf00
Allan <allanfelipedk@gmail.com>
2022-05-11 13:50:02 -0300

    Comando de laço While

---
+++ Comando de laço While/Comando de laço While/Comando de laço While.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numero = 0;
+	while (numero < 11)
+	{
+		cout << numero << "\n";
+		numero++;
+	}
+// 	while (numero++ < 10)
+// 	{
+// 		cout << numero << "\n";
+// 
+// 	}
+
+    system("pause>0");
+}
+
