c706d1d5ee5c18c3242a6385dee7466007517886
Allan <allanfelipedk@gmail.com>
2022-05-13 17:52:49 -0300

    Comando de loop Do While

---
+++ Comando de loop Do While/Comando de loop Do While/Comando de loop Do While.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    do 
+    {
+        cout << num << "\n";
+        num++;
+    } while (num < 10);
+
+
+
+    system("pause>0");
+
+}
