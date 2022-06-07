065e9f25590320defd9f0f5cc3e622fcfa4ff0e8
Allan <allanfelipedk@gmail.com>
2022-06-07 13:41:48 -0300

    Omissão de argumentos e argumentos padrão

---
+++ Omissão de argumentos e argumentos padrão/Omissão de argumentos e argumentos padrão/Omissão de argumentos e argumentos padrão.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void ReadText(string text = "Argumento");
+
+int main()
+{
+    ReadText();
+    system("pause>0");
+}
+void ReadText(string text)
+{
+    cout << "\n" << text << "\n";
+}
+
