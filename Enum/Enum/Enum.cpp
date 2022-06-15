364d7f209881a868b4c8239579d44c21266bd8db
Allan <allanfelipedk@gmail.com>
2022-06-15 19:46:51 -0300

    Enum

---
+++ Enum/Enum/Enum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    enum guns {fuzil = 100, revolver = 8, escopeta = 2, rifle = 12 };
+
+    guns armaSelecionada;
+
+    int num;
+
+    armaSelecionada = fuzil;
+    cout << armaSelecionada;
+
+    system("pause>0");
+}
+
